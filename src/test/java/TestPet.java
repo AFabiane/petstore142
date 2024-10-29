2bdb7456d7d74aaf3f7f2e57551b06eacef830f0
AFabiane <fabisn1987@gmail.com>
2024-10-28 22:39:52 -0300

    Criado Get da entidade Pet

--- src/test/java/TestPet.java
+++ src/test/java/TestPet.java
@@ -1,36 +1,62 @@
 // 0 - nome do pacote
-
+ 
 // 1 - bibliotecas
-
-
-
-import org.codehaus.groovy.transform.sc.StaticCompilationMetadataKeys;
+ 
+import io.restassured.response.Response; // Classe Resposta do REST-assured
+ 
 import org.junit.jupiter.api.Test;
-
-// 2 classe
+ 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+ 
+import static io.restassured.RestAssured.given; // função given
+import static org.hamcrest.Matchers.*;          // Classe de verificadores do Hamcrest
+ 
+// 2 - classe
 public class TestPet {
     // 2.1 atributos
     static String ct = "application/json"; // content-type
     static String uriPet = "https://petstore.swagger.io/v2/pet";
-
-    // 2.2 - funções e métodos
-    // 2.2.1 - funções  e métodos comuns / uteis
-    
-    // Função de leitura de Json    
-    public static String lerArquivoJson(String arquivoJson){
-        //TODO: Completar a leitura do arquivo
-        //return new String(Files)
-        return null;
+ 
+    // 2.2 funções e métodos
+    // 2.2.1 funções e métodos comuns / uteis
+ 
+    // Função de leitura de Json
+    public static String lerArquivoJson(String arquivoJson) throws IOException{
+        return new String(Files.readAllBytes(Paths.get(arquivoJson)));  
     }
-
-
-    // 2.2.2 - métodos de teste
+ 
+    // 2.2.2 métodos de teste
     @Test
-    public void testPostPet(){
+    public void testPostPet() throws IOException{
+        // Configura
+ 
         // carregar os dados do arquivo json do pet
-        // TODO: Criar o método Post
-        //String jsonBody = 
-
-
+        String jsonBody = lerArquivoJson("src/test/resources/json/pet1.json");
+       
+        int petId = 115293301; // código esperado do pet
+ 
+        // começa o teste via REST-assured
+ 
+        given()                                 // Dado que
+            .contentType(ct)                    // o tipo do conteúdo é
+            .log().all()                        // mostre tudo na ida
+            .body(jsonBody)                     // envie o corpo da requisição
+        // Executa    
+        .when()                                 // Quando
+            .post(uriPet)                       // Chamamos o endpoint fazendo post
+        // Valida
+        .then()                                 // Então
+            .log().all()                        // mostre tudo na volta
+            .statusCode(200) // verifique se o status code é 200
+            .body("name", is("Snoopy"))    // verifica se o nome é Snoopy
+            .body("id", is(petId))         // verifique o código do pet
+            .body("category.name", is("cachorro")) // se é cachorro
+            .body("tags[0].name", is("vacinado"))  // se está vacinado
+        ; // fim do given
+ 
     }
+ 
 }
+ 
