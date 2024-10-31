3f4a2840f98099cc1ee764dcbb3e0bca23fbd2fa
AFabiane <fabisn1987@gmail.com>
2024-10-30 22:53:49 -0300

    Teste massa

---
+++ src/test/java/Pet.java
@@ -0,0 +1,10 @@
+public class Pet {
+    // Estamos definindo uma classe chamada Pet para guardar a estrutura de dados 
+    // sobre os animais (pets)
+    public int petId;
+    public String petName;
+    public int catId;
+    public String catName;
+    public String status;
+    
+}
--- src/test/java/TestPet.java
+++ src/test/java/TestPet.java
@@ -2,22 +2,36 @@
  
 // 1 - bibliotecas
  
-import io.restassured.response.Response; // Classe Resposta do REST-assured
- 
-import org.junit.jupiter.api.Test;
- 
+import static io.restassured.RestAssured.config;
+import static io.restassured.RestAssured.given; // função given
+// Classe de verificadores do Hamcrest
+import static org.hamcrest.Matchers.is;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
- 
-import static io.restassured.RestAssured.given; // função given
-import static org.hamcrest.Matchers.*;          // Classe de verificadores do Hamcrest
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import com.google.gson.Gson;
+
  
 // 2 - classe
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class TestPet {
     // 2.1 atributos
     static String ct = "application/json"; // content-type
     static String uriPet = "https://petstore.swagger.io/v2/pet";
+    static int petId = 115293301;
+    String petName = "Snoopy";
+    String categoryName = "cachorro";
+    String tagName = "vacinado";
+    String[] status = {"available","sold"};
  
     // 2.2 funções e métodos
     // 2.2.1 funções e métodos comuns / uteis
@@ -28,15 +42,13 @@ public class TestPet {
     }
  
     // 2.2.2 métodos de teste
-    @Test
+    @Test @Order(1)
     public void testPostPet() throws IOException{
         // Configura
  
         // carregar os dados do arquivo json do pet
         String jsonBody = lerArquivoJson("src/test/resources/json/pet1.json");
-       
-        int petId = 115293301; // código esperado do pet
- 
+      
         // começa o teste via REST-assured
  
         given()                                 // Dado que
@@ -50,13 +62,142 @@ public class TestPet {
         .then()                                 // Então
             .log().all()                        // mostre tudo na volta
             .statusCode(200) // verifique se o status code é 200
-            .body("name", is("Snoopy"))    // verifica se o nome é Snoopy
+            .body("name", is(petName))    // verifica se o nome é Snoopy
             .body("id", is(petId))         // verifique o código do pet
-            .body("category.name", is("cachorro")) // se é cachorro
-            .body("tags[0].name", is("vacinado"))  // se está vacinado
+            .body("category.name", is(categoryName)) // se é cachorro
+            .body("tags[0].name", is(tagName))  // se está vacinado
         ; // fim do given
  
     }
- 
-}
+    @Test @Order(2)
+    public void testGetPet(){
+        // Configura
+        // Entrada - petId que está definido no nível da classe
+        
+            
+            given()
+                .contentType(ct)
+                .log().all()
+            .header("", "api_key: " + TestUser.testLogin())
+                // quando é get ou delet não tem body
+            // Executa
+            .when()
+                .get(uriPet + "/" + petId)  // montar o endpoint da URI/<petId>
+            // Valida
+            .then()
+                .log().all()
+                .statusCode(200) // verifique se o status code é 200
+                .body("name", is(petName))    // verifica se o nome é Snoopy
+                .body("id", is(petId))         // verifique o código do pet
+                .body("category.name", is(categoryName)) // se é cachorro
+                .body("tags[0].name", is(tagName))  // se está vacinado
+
+            ; // fim do given
+
+    }
+    @Test @Order(3)
+    public void testPutPet() throws IOException{
+        // Configura
+        String jsonBody = lerArquivoJson("src/test/resources/json/pet2.json");
+
+        given()
+            .contentType(ct)
+            .log().all()
+            .body(jsonBody)
+        // Executa
+        .when()
+            .put(uriPet)
+        // Valida
+        .then()
+            .log().all()
+            .statusCode(200)
+            .body("name", is(petName))    // verifica se o nome é Snoopy
+            .body("id", is(petId))         // verifique o código do pet
+            .body("category.name", is(categoryName)) // se é cachorro
+            .body("tags[0].name", is(tagName))  // se está vacinado
+            .body("status", is(status[1])) // status pet na loja
+        ;
+    }
+
+    @Test @Order(4)
+    public void testDeletPet(){
+        // Configura  -  Dados de entrada e saída no começo da classe
+
+        given()
+            .contentType(ct)
+            .log().all()
+
+        // Exexuta
+        .when()
+            .delete(uriPet + "/" + petId)
+        // Valida
+        .then()
+            .log().all()
+            .statusCode(200)   // se comunicou e processou
+            .body("code", is(200))           // se apagou
+            .body("type", is("unknown"))
+            .body("message", is(String.valueOf(petId)))
+        ;
+    }
+
+    // Data Data Testing (DDT) / Teste direcionado por Dados / Teste com Massa
+    // Teste com Json parametrizado
+
+    @ParameterizedTest @Order(5)
+    @CsvFileSource(resources = "/csv/petMassa.csv", numLinesToSkip = 1, delimiter = ',')
+    public void testPostPetDDT(
+       int petId,
+       String petName,
+       int catId,
+       String catName,
+       String status1,
+       String status2
+
+    ) // fim dos parametros 
+    { // inicio do código do método testPostPetDDt
+
+        // Criar a classe pet para receber o dados do csv
+        Pet pet = new Pet();  // Instancia a classe User
+        
+        pet.petId = petId;
+        pet.petName = petName;
+        pet.catId = catId;
+        pet.catName = catName;
+        pet.status = status1;
+    
+        // Criar um Json para o Body a ser enviado a partir da classe Pet e do CSV
+        Gson gson = new Gson();  // Instancia a classe Gson como o objeto gson 
+        String jsonBody = gson.toJson(pet);
+
+        given()
+            .contentType(ct)
+            .log().all()
+            .body(jsonBody)
+        .when()
+            .post(uriPet)
+        .then()
+            .log().all()
+            .statusCode(200)
+            .body("id", is(petId))
+            .body("name", is(petName))
+            .body("category.id", is(catId))
+            .body("category.name", is(catName))
+            .body("statusCode", is(200))
+
+        
+
+     
+
+            ;
+
+
+    }
+
+
+ }
+
+  
+
+
+
  
